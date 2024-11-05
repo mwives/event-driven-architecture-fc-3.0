2d5ee1d640347e881d2edcc750059911bb34734d
Ives M. W <ivesmw@gmail.com>
2024-11-05 17:00:54 -0300

    fix: parse created_at to time

--- internal/database/client_db.go
+++ internal/database/client_db.go
@@ -2,6 +2,8 @@ package database
 
 import (
 	"database/sql"
+	"errors"
+	"time"
 
 	"github.com/mwives/microservices-fc-walletcore/internal/entity"
 )
@@ -18,13 +20,20 @@ func NewClientDB(db *sql.DB) *ClientDB {
 
 func (c *ClientDB) FindByID(ID string) (*entity.Client, error) {
 	client := &entity.Client{}
+	var createdAtBytes []byte
 
 	err := c.DB.
 		QueryRow("SELECT id, name, email, created_at FROM clients WHERE id = ?", ID).
-		Scan(&client.ID, &client.Name, &client.Email, &client.CreatedAt)
+		Scan(&client.ID, &client.Name, &client.Email, &createdAtBytes)
 	if err != nil {
 		return nil, err
 	}
+
+	createdAt, err := time.Parse("2006-01-02 15:04:05", string(createdAtBytes))
+	if err != nil {
+		return nil, errors.New("error parsing created_at column to time.Time")
+	}
+	client.CreatedAt = createdAt
 
 	return client, nil
 }
