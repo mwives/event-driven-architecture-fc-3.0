4b68efc3f0f23f80364dc6f22863f71bf210629d
Ives M. W <ivesmw@gmail.com>
2024-11-05 19:35:39 -0300

    feat: add UpdateBalance method

--- internal/gateway/account.go
+++ internal/gateway/account.go
@@ -5,4 +5,5 @@ import "github.com/mwives/microservices-fc-walletcore/internal/entity"
 type AccountGateway interface {
 	Create(account *entity.Account) error
 	FindByID(id string) (*entity.Account, error)
+	UpdateBalance(account *entity.Account) error
 }
