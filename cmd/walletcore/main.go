7280212e8251c543c784de6bcd8627e94a19bb52
Ives M. W <ivesmw@gmail.com>
2024-11-06 10:31:33 -0300

    feat: implement Kafka handler for transaction created events and update main application logic

--- cmd/walletcore/main.go
+++ cmd/walletcore/main.go
@@ -5,31 +5,40 @@ import (
 	"database/sql"
 	"fmt"
 
+	ckafka "github.com/confluentinc/confluent-kafka-go/kafka"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/mwives/microservices-fc-walletcore/internal/database"
 	"github.com/mwives/microservices-fc-walletcore/internal/event"
+	"github.com/mwives/microservices-fc-walletcore/internal/event/handler"
 	"github.com/mwives/microservices-fc-walletcore/internal/usecase/create_account"
 	"github.com/mwives/microservices-fc-walletcore/internal/usecase/create_client"
 	"github.com/mwives/microservices-fc-walletcore/internal/usecase/create_transaction"
 	"github.com/mwives/microservices-fc-walletcore/internal/web"
 	"github.com/mwives/microservices-fc-walletcore/internal/web/webserver"
 	"github.com/mwives/microservices-fc-walletcore/pkg/events"
+	"github.com/mwives/microservices-fc-walletcore/pkg/kafka"
 	"github.com/mwives/microservices-fc-walletcore/pkg/uow"
 )
 
 func main() {
 	db, err := sql.Open("mysql", fmt.Sprintf(
 		"%s:%s@tcp(%s:%s)/%s",
-		"root", "root", "localhost", "3306", "wallet",
+		"root", "root", "mysql", "3306", "wallet",
 	))
 	if err != nil {
 		panic(err)
 	}
 	defer db.Close()
 
+	configMap := ckafka.ConfigMap{
+		"bootstrap.servers": "kafka:29092",
+		"group.id":          "wallet",
+	}
+	kafkaProducer := kafka.NewKafkaProducer(&configMap)
+
 	eventDispatcher := events.NewEventDispatcher()
+	eventDispatcher.Register("transaction_created", handler.NewTransactionCreatedKafkaHandler(kafkaProducer))
 	transactionCreatedEvent := event.NewTransactionCreatedEvent()
-	// eventDispatcher.Register("transaction_created", handler)
 
 	clientDB := database.NewClientDB(db)
 	accountDB := database.NewAccountDB(db)
